07d504515456054c616fafbf952c7d86f0a14927
Oliver-Rose-University <oliver.rose@student.norwichuni.ac.uk>
2024-10-31 14:08:13 +0000

    push push push your boat, gently down the stream

---
+++ script.js
@@ -0,0 +1,41 @@
+document.getElementById("humidity-slider").disabled = true;
+document.getElementById("wind-slider").disabled = true;
+document.getElementById("temp-slider").disabled = true;
+
+const humidityAmount = document.getElementById("humidity-amount");
+const windAmount = document.getElementById("wind-amount");
+const tempAmount = document.getElementById("temp-amount");
+const humiditySlider = document.getElementById("humidity-slider");
+const windSlider = document.getElementById("wind-slider");
+const tempSlider = document.getElementById("temp-slider");
+
+const apiUrl = "http://localhost:5500/test.json";
+
+async function fetchData(){
+    try{
+        const response = await fetch(apiUrl);
+        if(!response.ok){
+            throw new Error("Response Status: ", response.status);
+        }
+        const json = await response.json();
+        console.log(json);
+        updateData(json);
+    } catch(error){
+        console.error(error);
+    }
+    
+}
+
+
+
+function updateData(json) {
+    humidityAmount.innerHTML = ("The current Humidity is: " + json.current.humidity + "%!");
+    humiditySlider.value = (json.current.humidity);
+    windAmount.innerHTML = ("The current Wind Speed is: " + json.current.wind_speed + "/Mph!");
+    windSlider.value = (json.current.wind_speed);
+    tempAmount.innerHTML = (json.current.temperature + "ºC");
+    tempSlider.value = (json.current.temperature);
+}
+
+
+setInterval(fetchData, 500);
